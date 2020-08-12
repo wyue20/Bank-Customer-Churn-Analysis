ab98905c0c1141f7b477f09fdd254c9bd32682a2
wyue20 <59752517+wyue20@users.noreply.github.com>
2020-08-12 01:46:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -118,6 +118,4 @@ def main_search():
         return  render_template("index.html")
 
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 5000))
-    
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run()
