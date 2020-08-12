89987bc163c91ef260d432c7a2fc0dae5f429a1b
wyue20 <59752517+wyue20@users.noreply.github.com>
2020-08-12 01:40:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from weibo import APIClient 
 import requests
 import json
-import urllib.request #导入urllib.request库
+import urllib.request 
 import urllib.parse
 from lxml import etree
 import io
@@ -118,4 +118,6 @@ def main_search():
         return  render_template("index.html")
 
 if __name__ == '__main__':
-    app.run()
+    PORT = int(os.environ.get('PORT', 5000))
+    
+    app.run(host='0.0.0.0', port=port, debug=True)
